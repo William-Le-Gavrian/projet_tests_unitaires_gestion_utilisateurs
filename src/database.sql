b9d497d415b225c61f6927d95d5946b805a92b6c
William LG <103275212+William-Le-Gavrian@users.noreply.github.com>
2025-03-23 17:05:51 +0100

    Delete src/database.sql

--- src/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE user_management;
-
-USE user_management;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE
-);
