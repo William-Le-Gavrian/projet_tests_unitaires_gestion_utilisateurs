6383ef6146d3d5bf020af1d231dccba7963200a4
bidoumou <williamlg@free.fr>
2025-04-05 19:09:55 +0200

    redo tests-k6.js

--- src/tests-k6.js
+++ src/tests-k6.js
@@ -1,5 +1,5 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { sleep, check } from 'k6';
 
 export const options = {
     vus: 500,
@@ -7,21 +7,16 @@ export const options = {
 };
 
 export default function () {
-    const url = 'http://127.0.0.1:8000/api.php';
 
-    const payload = JSON.stringify({
+    const url = 'http://localhost/projet_tests_unitaires_gestion_utilisateurs/api.php';
+
+    let data = {
         name: `User${Math.random().toString(36).substr(2, 5)}`,
         email: `user_${Math.random().toString(36).substr(2, 5)}@example.com`,
         age: `${Math.floor(Math.random() * 101)}`
-    });
-
-    const params = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
     };
 
-    const response = http.post(url, payload, params);
+    const response = http.post(url, data);
 
     check(response, {
         'Statut 200': (r) => r.status === 200,
